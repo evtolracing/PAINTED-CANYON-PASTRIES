d7721054cc353baf68a696d49a2ce02cddbaa49d
evtolracing <evtolracing@users.noreply.github.com>
2026-02-23 18:32:26 -0800

    Debug: wrap app require in try/catch to expose startup errors

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 require('dotenv').config({ path: require('path').join(__dirname, '..', 'server', '.env') });
-const app = require('../server/src/app');
+
+let app;
+try {
+  app = require('../server/src/app');
+} catch (err) {
+  // Expose startup error for debugging
+  app = (req, res) => {
+    res.status(500).json({ error: 'Startup failed', message: err.message, stack: err.stack });
+  };
+}
 
 module.exports = app;
